eb4bcf84047cc19f472ca69b077cd110ca981826
DrugzCartel <95879295+DrugzCartel@users.noreply.github.com>
2022-09-12 01:24:11 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,14 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-
+<iframe
+src="https://gateway.ipfscdn.io/ipfs/Qma5FgmS9s3hkQrURfG4XDnh5Dxp9Fx9ZjBYkPFUNdQwfi/nft-drop.html?contract=0x346E7C0d8e8e0CEC7aF773CC022788301a829e0e&chainId=80001"
+width="600px"
+height="600px"
+style="max-width:100%;"
+frameborder="0"
+></iframe>
+    
       <Footer />
     </div>
   )
